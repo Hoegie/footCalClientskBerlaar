1b1a1e6fc0edd6f7d1cd45c48c3dcb760ca22975
Hoegie <mezenhof8@gmail.com>
2018-06-06 09:08:59 +0200

    nth commit

--- footcal.js
+++ footcal.js
@@ -321,6 +321,7 @@ console.log(teamID);
             title = "club_annulation";
           }
         var locTitle = androidtranslator[row.device_language][title];
+        locTitle = locTitle.replace("%1", clubName);
         var body = androidtranslator[row.device_language][eventType];
         body = body.replace("%1", date);
         body = body.replace("%2", teamName);
