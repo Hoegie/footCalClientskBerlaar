4fccf421541b89ed2bedd9945ac7afc5b987b90d
Hoegie <mezenhof8@gmail.com>
2018-07-11 09:57:10 +0200

    nth commit

--- footcal.js
+++ footcal.js
@@ -3,7 +3,6 @@ var express    = require('express');
 var mysql      = require('mysql');
 var bodyParser = require('body-parser');
 var apn = require('apn');
-//var gcm = require('node-gcm');
 var fcm = require('fcm-push');
 var nodemailer = require('nodemailer');
 var ejs = require('ejs');
