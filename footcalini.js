b9a41e74078f7b48c1fe84d223553308f4f2e0cc
Hoegie <mezenhof8@gmail.com>
2019-06-09 12:13:26 +0200

    nth commit

--- footcalini.js
+++ footcalini.js
@@ -3,7 +3,7 @@ module.exports.clubName = 'SK BERLAAR';
 module.exports.clubBaseNr = '9375';
 module.exports.apisec = false;
 module.exports.dbname = 'FootCal_skBerlaar_dev';
-module.exports.apachedir = 'skBerlaar';
+module.exports.apachedir = 'skBerlaar_dev';
 module.exports.apiport = 4001;
 module.exports.serveraddress = "185.224.89.139";
 
