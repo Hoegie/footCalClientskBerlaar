db5c3de3559c34600852677239d0175a0b9e8aa1
Hoegie <mezenhof8@gmail.com>
2019-12-10 07:24:11 +0100

    nth commit

--- footcal.js
+++ footcal.js
@@ -1,4 +1,4 @@
-//LIVE VERSION 7 (dayoverview) go live version  
+//LIVE VERSION 8 Dashboard adapted 
 var express    = require('express');
 var mysql      = require('mysql');
 var bodyParser = require('body-parser');
